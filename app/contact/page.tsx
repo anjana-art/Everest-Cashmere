3f7ba5763549901321a4db559bb1b66eb07d7d4b
anjana-art <68542773+anjana-art@users.noreply.github.com>
2026-01-09 13:15:52 +0000

    fix error

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,8 @@
-import Button from "../button/page"
 export default function Contact(){
         console.log('hey is this on client or in server ...')
 
     return <div>
         Contact Us
-        <Button/>
 
     </div>
 }
