327017443bbcd47a99756eb357302ee8869cc1c0
anjana-art <68542773+anjana-art@users.noreply.github.com>
2026-01-15 13:06:22 +0000

    teste version control

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,9 @@ export default async function Home(){
             <div className="mx-auto grid grid-cols-1 items-center justify-items-center gap-8 px-8 sm:px-16  md:grid-cols-2">
                <div className="max-w-md space-y-4">
                 <h1 className="text-3xl font-bold tracking-tight md:text-4xl text-red-900">Welcome to Everesté!</h1>
-                <p  className="text-neutral-600">Discover the timeless elegance with style.Pure Cashmere. handmade with love,  wrapped under the mountain and  direct to  your closet . suppor sustanable , luxury and timeless fashion from Deepanjana. because we care  Enviroment friendly, local artician and your comfort along with style. </p>
+                <p  className="text-neutral-600">Discover the timeless elegance with style.Pure Cashmere. handmade with love, 
+                   wrapped under the mountain and  direct to  your closet . suppor sustanable , luxury and timeless fashion from 
+                   Everesté. because we care  Enviroment friendly, local artician and your comfort along with style. </p>
                
                <Button asChild className=" text-2xl inline-flex items-center justify-center rounded-full px-6 py-3 text-white bg-amber-600 hover:bg-amber-500 "
 >
